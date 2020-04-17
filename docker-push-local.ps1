be4be8b50d9db5b8dab350d148cf6ef056169e4c
mvonhagt <63381618+mvonhagt@users.noreply.github.com>
2020-04-17 14:34:51 +1000

    Update docker-push-local.ps1

--- docker-push-local.ps1
+++ docker-push-local.ps1
@@ -1,4 +1,22 @@
+param ($Repository, $BuildNumber)
+
+if ($Repository -eq $null) {
+$DockerRepository = "mvonhagt/pos_testharness:"
+}Else{
+$DockerRepository = $Repository
+}
+
+if ($BuildNumber -eq $null) {
+$DockerRepository = $Repository+":latest"
+}Else{
+$DockerRepository = $Repository+$BuildNumber
+}
+
 docker login -u mvonhagt -p mv1209v12
-docker tag pos_testharness:local mvonhagt/pos_testharness:latest
-docker push mvonhagt/pos_testharness:latest
-docker rmi mvonhagt/pos_testharness:latest
+# docker tag pos_testharness:local mvonhagt/pos_testharness:latest
+# docker push mvonhagt/pos_testharness:latest
+# docker rmi mvonhagt/pos_testharness:latest
+
+docker tag pos_testharness:local $DockerRepository
+docker push $DockerRepository
+docker rmi $DockerRepository
