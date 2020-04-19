0dd2e8fda7be9f54fc8263db092a7d6ed301c1c2
mvonhagt <63381618+mvonhagt@users.noreply.github.com>
2020-04-19 12:10:17 +1000

    Update docker-push-local.ps1

--- docker-push-local.ps1
+++ docker-push-local.ps1
@@ -1,8 +1,4 @@
 param ($Repository, $BuildNumber,$DockerUser, $DockerPwd  )
-write-host $Repository
-write-host $BuildNumber
-write-host $DockerUser
-write-host $DockerPwd
 
 ## Convert the user and password from secret to redable form
 $DockerUser = [System.Text.Encoding]::UTF8.GetBytes($DockerUser)
@@ -13,12 +9,9 @@ $DockerPwd  = [System.Text.Encoding]::UTF8.GetBytes($DockerPwd )
 $DockerPwd  = [System.Convert]::ToBase64String($DockerPwd )
 $DockerPwd  = [System.Convert]::FromBase64String($DockerPwd)
 $DockerPwd =  [System.Text.Encoding]::UTF8.GetString($DockerPwd)
+
 write-host $DockerUser
 write-host $DockerPwd
-
-# $DockerEncPassword = ConvertTo-SecureString "$DockerPwd" -AsPlainText -Force
-# $Credential = New-Object System.Management.Automation.PSCredential ($DockerUser, $DockerEncPassword)
-# Invoke-Command -ComputerName node1 -Credential $Credential -ScriptBlock {Get-Process}
 
 if ($Repository -eq $null) {
 $DockerRepository = "mvonhagt/pos_testharness:"
@@ -32,7 +25,6 @@ $DockerRepository = $Repository+":latest"
 $DockerRepository = $Repository+":"+$BuildNumber
 }
 
-# docker login -u mvonhagt -p mv1209v12
 docker login -u $DockerUser -p $DockerPwd
 # docker tag pos_testharness:local mvonhagt/pos_testharness:latest
 # docker push mvonhagt/pos_testharness:latest
